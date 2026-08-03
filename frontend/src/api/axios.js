4eddf187a9ebb3330ba3f6a14f80887ec16c07c6
Ratankrjha <ratankrjha.info@gmail.com>
2026-08-03 21:44:22 +0530

    Fix frontend auth header: send Bearer token in axios request interceptor

--- frontend/src/api/axios.js
+++ frontend/src/api/axios.js
@@ -1,4 +1,4 @@
-import axios from "axios";
+﻿import axios from "axios";
 
 const API_BASE_URL = import.meta.env.VITE_API_URL || "http://localhost:5000/api";
 
@@ -9,7 +9,7 @@ const api = axios.create({
 api.interceptors.request.use((config) => {
   const token = localStorage.getItem("token");
   if (token) {
-    config.headers.Authorization = `Bearer ${token}`;
+    config.headers.Authorization = "Bearer " + token;
   }
   return config;
 });
